e1cb9b9dab5622978a77cb1f45afe17b945328ce
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-04-15 13:11:43 +0200

    working execute method

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
-from flask import Flask
+import pm4py
 import pm4pytool
+from pm4pytool import mapping
+from pm4pytool.mapping import Mapping
+from flask import Flask, request, jsonify, abort
+from flask_cors import CORS
+import uuid
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder="html")
+app.add_url_rule(app.static_url_path + '/<path:filename>', endpoint='static',
+                 view_func=app.send_static_file)
+CORS(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def real_execute(method, args, kwargs):
+    after_exec = method(*args, **kwargs)
+    res = {"objects": [], "algoResult": {}}
+    if type(after_exec) is tuple or type(after_exec) is list:
+        for obj in after_exec:
+            Mapping.obj_map[str(id(obj))] = after_exec
+            obj_dict = {str(id(obj)): mapping.synth_obj(obj)}
+            res["objects"].append(obj_dict)
+        Mapping.obj_map[str(id(after_exec))] = after_exec
+        syn = mapping.synth_algo(method, after_exec)
+        res["algoResult"] = {"id": str(id(after_exec)), "type": syn[0], "descr": syn[1]}
+    else:
+        Mapping.obj_map[str(id(after_exec))] = after_exec
+        obj_dict = {str(id(after_exec)): mapping.synth_obj(after_exec)}
+        res["objects"].append(obj_dict)
+        res["algoResult"] = {"id": str(id(after_exec)), "type": str(type(after_exec)), "descr": mapping.synth_obj(after_exec)["repr"]}
+    return res
 
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    content = json.loads(request.data)
+    session = request.cookies.get('session') if 'session' in request.cookies else str(uuid.uuid4())
+    method = content["method"]
+    args = content["args"]
+    kwargs_id = content["kwargs"]
+    kwargs = {x.split("==")[0]: x.split("==")[1] for x in kwargs_id}
+    for i in range(len(args)):
+        args[i] = pm4pytool.mapping.get_arg(session, args[i])
+    for x in kwargs.keys():
+        kwargs[x] = pm4pytool.mapping.get_arg(session, kwargs[x])
+    method = eval(method)
+    args = tuple(args)
+    kwargs = dict(kwargs)
+    res = real_execute(method, args, kwargs)
+    response = jsonify(res)
+    if not request.cookies.get('session'):
+        response.set_cookie('session', session)
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- pm4pytool/mapping.py
+++ pm4pytool/mapping.py
@@ -3,9 +3,24 @@ class Mapping:
     obj_synthesis = {}
     representation = {}
     obj_map = {}
+    obj_session_map = {}
 
 
 def synth_obj(obj):
     if type(obj) in Mapping.obj_synthesis:
-        return Mapping.obj_synthesis[type(obj)](obj)
-    return repr(obj)
+        return {"repr": Mapping.obj_synthesis[type(obj)](obj), "type": str(type(obj))}
+    return {"repr": repr(obj), "type": str(type(obj))}
+
+
+def synth_algo(algo, after_exec):
+    if algo in Mapping.algo_synthesis:
+        return Mapping.algo_synthesis[algo](after_exec)
+    return "", "Object: "+str(id(after_exec))
+
+
+def get_arg(session, id):
+    if id in Mapping.obj_map:
+        return Mapping.obj_map[id]
+    elif session in Mapping.obj_session_map and id in Mapping.obj_session_map[session]:
+        return Mapping.obj_session_map[session][id]
+    return id
