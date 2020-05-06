1764052126128fcf9d36063cf42721664edfc0f7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-05-06 08:48:40 +0200

    added support for tabs

--- html/js/app-main.js
+++ html/js/app-main.js
@@ -4,6 +4,59 @@ let objMapping = {};
 let algoMapping = {};
 let objNames = {};
 
+
+Vue.component('tab', {
+
+    template: '<div v-show="isActive"><slot></slot></div>',
+
+    props: {
+        name: { required: true },
+        selected: { default: false}
+    },
+
+    data() {
+
+        return {
+            isActive: false
+        };
+
+    },
+
+    computed: {
+
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+
+        this.isActive = this.selected;
+
+    }
+});
+
+Vue.component('tabs', {
+    template: '<div><div class="tabs"><ul><li v-for="tab in tabs"><a v-on:click="selectTab(tab)" href="#">{{tab.name}}</a></li></ul></div><div class="tabs-details"><slot></slot></div></div>',
+
+    data() {
+        return {tabs: [] };
+    },
+
+    created() {
+
+        this.tabs = this.$children;
+
+    },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+});
+
 const App = new Vue({
   el: '#app',
   data: {
