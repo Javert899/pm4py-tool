cb0b1f70ce60e9917689065d8fe75eac9c134113
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-05-18 15:18:29 +0200

    updating objects

--- html/js/pm4py-calls.js
+++ html/js/pm4py-calls.js
@@ -14,7 +14,7 @@ function AjaxCall(url, data) {
       return res;
 }
 
-function Execute(method, args0, kwargs0, suggestedType="", retAlgo=true) {
+function Execute(method, args0, kwargs0, suggestedType="", retAlgo=true, updatedObj=null) {
     let args = args0.slice();
     let kwargs = Object.assign({}, kwargs0);
     let i = 0;
@@ -30,14 +30,29 @@ function Execute(method, args0, kwargs0, suggestedType="", retAlgo=true) {
         }
     }
     let res = AjaxCall('/execute', {"method": method, "args": args, "kwargs": kwargs, "suggested_type": suggestedType}).responseJSON;
+    if (updatedObj != null) {
+
+        if (res["objects"].length == 1) {
+            res["objects"][0][1]["depending"] = updatedObj[1]["depending"];
+        }
+        //UpdateCallback(updatedObj);
+    }
     for (let idx in res["objects"]) {
         objMapping[res["objects"][idx][0]] = res["objects"][idx];
     }
     algoMapping[res["algoResult"][0]] = res["algoResult"];
     if (retAlgo) {
+        if (updatedObj != null) {
+            res["algoResult"][1]["depending"] = updatedObj[1]["depending"];
+            UpdateCallback(res["algoResult"]);
+        }
         return res["algoResult"];
     }
     if (res["objects"].length == 1) {
+        if (updatedObj != null) {
+            res["objects"][0][1]["depending"] = updatedObj[1]["depending"];
+            UpdateCallback(res["objects"][0]);
+        }
         return res["objects"][0];
     }
     return res["objects"];
