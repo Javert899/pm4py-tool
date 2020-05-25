5eed831c83ac548cd4c27e5228301f6f13de199d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-05-25 07:49:07 +0200

    objects view

---
+++ html/components/objects-view.js
@@ -0,0 +1,32 @@
+let ObjectsViewViewer = {
+    template: "<div><div v-for='value in intAlgoMapping'><b>ID:</b> {{value[0]}}<br /><b>Name:</b> {{value[1]['repr']}}<br /><b>Type:</b> {{value[1]['type']}}<br /><b>Creation Date:</b> {{value[1]['creationTimestamp']}}<br /><br /><br /></div></div>",
+    data: function() {
+        return {
+            name: '',
+            intAlgoMapping: {}
+        }
+    },
+    methods: {
+    }
+}
+
+function InitializeObjectsView(name="defaultObjectsView", target_comp=null) {
+    let objectsView =  Object.assign({}, ObjectsViewViewer);
+        if (target_comp == null) {
+        App.addChildren(name, objectsView);
+    }
+    else {
+        App.$emit("addComponentByName", [target_comp, name, objectsView]);
+    }
+    let comp = App.childrenMap[name];
+    console.log(algoMapping);
+    let updateFunction = function() {
+        return function() {
+            return {
+                name: name,
+                intAlgoMapping: algoMapping
+            }
+        }
+    }
+    objectsView.data = updateFunction();
+}
